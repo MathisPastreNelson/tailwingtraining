bc8a1d8c6022625122968cbe985be8aea5dea29b
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-07-03 20:45:57 +0200

    Training : OK

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="font-black text-primary bg-lol tablet:bg-primary tablet:text-black mobile:bg-secondlol mobile:text-primary w-80 mx-auto text-center">
+    class="font-black text-primary bg-lol tablet:bg-primary tablet:text-black mobile:bg-secondlol mobile:text-primary w-80 mobile:w-40 mx-auto text-center">
     <h1>header</h1>
   </header>
 </template>
--- components/Main.vue
+++ components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="sectionContainer flex justify-center items-center tablet:bg-secondlol mobile:bg-secondlol bg-lol">
+    class="sectionContainer flex justify-center items-center bg-[url('./public/images/herosection.svg')] tablet:bg-secondlol mobile:bg-lol bg-cover bg-center">
     Hello test
   </section>
 </template>
