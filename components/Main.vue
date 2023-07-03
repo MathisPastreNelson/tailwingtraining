2586cf3bd99501a7873fbc4490ee851c75c11b96
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-07-03 21:39:27 +0200

    training ok

--- components/Main.vue
+++ components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <section
     class="sectionContainer flex justify-center items-center bg-[url('./public/images/herosection.svg')] tablet:bg-secondlol mobile:bg-lol bg-cover bg-center">
-    Hello test
+    <h1 class="hover:underline cursor-pointer">Hello test</h1>
   </section>
 </template>
